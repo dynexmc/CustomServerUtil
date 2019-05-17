d433865c4d16efe56ed62b1482150406d960b32c
Ayley <larshatzky@gmail.com>
2019-05-17 21:14:35 +0200

    Update Setwarp and Warps

--- src/main/java/net/snapecraft/CustomServerUtilv2/commands/WarpCMD.java
+++ src/main/java/net/snapecraft/CustomServerUtilv2/commands/WarpCMD.java
@@ -1,5 +1,6 @@
 package net.snapecraft.CustomServerUtilv2.commands;
 
+import net.snapecraft.CustomServerUtilv2.main.CustomServerUtilv2;
 import net.snapecraft.CustomServerUtilv2.util.Locations;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -15,22 +16,33 @@ public class WarpCMD implements CommandExecutor {
             if(args.length == 0 && !cmd.getName().equalsIgnoreCase("warps")){
 
             }
+            //setWarp
             if(args.length == 1){
-                Locations loc = new Locations(player.getLocation(),args[0]);
-                if(!loc.warpExists()){
-                    loc.setWarp();
+                if(cmd.getName().equalsIgnoreCase("setWarp")){
+                    if(player.hasPermission("csu.setwarp")) {
+                        Locations loc = new Locations(player.getLocation(), args[0]);
+                        if (!loc.warpExists()) {
+                            loc.setWarp();
+                            player.sendMessage(CustomServerUtilv2.Prefix + "§2Du hast den Warp §6" + args[0] + " §2erstellt!");
+                        } else {
+                            player.sendMessage(CustomServerUtilv2.Prefix + "§4Der Warp existiert bereits!");
+                        }
+                    }else{
+                        player.sendMessage(CustomServerUtilv2.Prefix + "§4Du hast nicht die benötigten Rechte!");
+                    }
                 }
             }
+            //warps
             if(args.length == 0){
                 if(cmd.getName().equalsIgnoreCase("warps")){
                     Locations loc = new Locations();
-                    player.sendMessage(loc.waprList());
+                    player.sendMessage(CustomServerUtilv2.Prefix + "§2Warps: §6" + loc.warpList());
                 }
             }
 
             if(args.length == 1){
                 if(cmd.getName().equalsIgnoreCase("warp")){
-                    Locations locations = new Locations(args[0],0);
+                    Locations locations = new Locations(args[0]);
                     if(locations.warpExists()){
                         player.teleport(locations.getWarp());
                     }
